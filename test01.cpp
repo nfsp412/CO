5fc84b3147ce7f07ef92d6b7151f5e76eef70452
Asuka <nfsp412@163.com>
2025-05-28 09:08:12 +0800

    运算方法和运算电路

--- test01.cpp
+++ test01.cpp
@@ -28,6 +28,32 @@ int main()
     //字节数
     printf("%d\n",sizeof(short));//2
     printf("%d\n",sizeof(unsigned short));//2
+    printf("%d\n",sizeof(unsigned ));//4
+    printf("%d\n",sizeof(int));//4
+
+    int si = 65536;
+    short i = si;
+    unsigned j = 0;
+    if (i<=j-1)
+    {
+        printf("wangdao\n");//yes
+    }else{
+        printf("jisaunjijiaoyu\n");
+    }
+    
+    int xa=127;
+    short ya=-9;
+    int za;
+    za=xa+ya;
+    printf("%d\n",xa);//127
+    printf("%d\n",ya);//-9
+    printf("%d\n",za);//118
+
+    int ii=32777;
+    short ssi=i;
+    int jj=ssi;
+    printf("%d\n",ssi);//0
+    printf("%d\n",jj);//0
 
     return 0;
 }
